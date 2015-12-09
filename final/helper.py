e5c2230a50b1da3181748264c6359199b61eba7a
David Skoda <dskoda1@binghamton.edu>
2015-12-09 02:58:35 +0000

    taking random samples, need to cluster next

--- final/helper.py
+++ final/helper.py
@@ -3,35 +3,23 @@ import sys
 import csv
 import functools
 def readFile(fileName):
-#  f = open(fileName, 'rU')
-#  data = []
-#  try:
-#    reader = csv.reader(f)
-#    print('opened file')
-#    for row in reader:
-#      vec = []
-#      for x in row:
-#        vec.append(x)
-#      data.append(vec)
-#  except:
-#    print('unexpected error reading file: ' + str(sys.exc_info()[0]))
-#  finally:
-#    f.close()
- 
+  #Split lines from file
   lines = open(fileName).read().split("\n")
-  reader = csv.reader(lines, delimiter=' ')
+  reader = csv.reader(lines)
+  #Split points now
+  
   data = [ point for point in [line for line in reader]]
+  #Pop last row out, empty list
   data.pop()
   
-  fData =list( map(convertListToFloats, data))
-
-  print(fData)
+  #convert each list of string vals to a list of floats
+  fData = list( map(convertListToFloats, data))
+  return fData
   
 #Input contains some empty strings, seems like we need to clean first
 def convertListToFloats(row):
-  print(row)
-  return list(map(convertStringToFloat, row))
-
+  strings = row[0].split()
+  return list( map(convertStringToFloat, strings))
+#Helper 
 def convertStringToFloat(string):
-  print(string)
   return float(string)
--- final/main.py
+++ final/main.py
@@ -6,9 +6,11 @@ David Skoda
 Final Project
 Environment: Python 3
 '''
-
+from scipy.cluster.vq import *
 import sys
 from helper import *
+from scipy.spatial import distance
+import random
 
 #Main function
 if __name__ == '__main__':
@@ -16,4 +18,18 @@ if __name__ == '__main__':
   #define some variables
   fileName = 'data.txt'  
   data = readFile(fileName)
+  
+  #Get the user input 'k' value
+  k = int(sys.argv[1])
+  
+  #select k random data points
+  random.seed()
+  centroids = random.sample(data, k)
+  for x in centroids:
+      print(x)
+  
+  
+  
+  
+  # print(distance.euclidean(data[1], data[4]))
 
