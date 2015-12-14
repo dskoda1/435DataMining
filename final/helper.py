48e6d6480ef37e93c3e2ce2b8b44eddcdc2dc0b4
David Skoda <dskoda1@binghamton.edu>
2015-12-14 01:32:10 +0000

    kMeans algo is working, have basic accuracy calc as well. Program stable

--- final/helper.py
+++ final/helper.py
@@ -7,84 +7,75 @@ import copy
 from scipy.spatial import distance
 import numpy
 
-
-#Main function that glues together algorithms parts
-def kMeans(data, k, runs):
+def kMeans(data, k):
   #select k random data points
   random.seed()
   centroids = random.sample(data, k)
-  oldCentroids = copy.deepcopy(centroids)
-  
-  #Assign each vector to an initial cluster, save as array of dict like
-  #{'vec': [x1, x2, x3..], 'cluster': kx, 'class': ky}
-  mappedVecs = [assignToCluster(vec, centroids, i) for i, vec in enumerate(data)]
-  #reassign each vector the specified amount of times, 
-  #recomputing the average of each cluster every run
-  track = []
-  for x in mappedVecs:
-    track.append(x['cluster'])
-    
-  print(track[:5])
-  for x in range(15):
-    didChange = False
-    print([distance.euclidean(centroids[i], oldCentroids[i]) for i in range(0, k)])
+  mappedVecs = [copy.deepcopy(assignToCluster(vec, centroids, i)) for i, vec in enumerate(data)]
 
-    centroids = list(copy.deepcopy([getAverageOfCluster(mappedVecs, i) for i in range(0, k)]))
-    oldCentroids = list(copy.deepcopy(centroids))
-    mappedVecs = list(copy.deepcopy([reassignToCluster(vec, centroids) for vec in mappedVecs]))
+  while True:
+    #Get new centroids
+    centroids = copy.deepcopy([copy.deepcopy(getAverageOfCluster(mappedVecs, i)) for i in range(0, k)])
+    mappedVecs = copy.deepcopy([copy.deepcopy(reassignToCluster(vec, centroids)) for vec in mappedVecs])
+    numChanged = len(list(filter(lambda vec: vec['change'], mappedVecs)))
+    if numChanged == 0:
+      break;
     
-    # for i, x in enumerate(mappedVecs):
-    #   if track[i] != x['cluster']:
-    #     didChange = True
-    #     track[i] = x['cluster']
-    # if  not didChange:
-    #   break
-    #numChanged = len(list(filter(lambda vec: (vec['change'] == True), mappedVecs)))
-    #print(numChanged)
-    # for vec in numChanged:
-    #   print('id:' + str(vec['class']))
-    #   print('cluster:' + str(vec['cluster']))
-    # print('\n\n')
-    #if numChanged == 0:
-    #  break
-
-  
-  #Return the vectors and their classes in a dict
   return mappedVecs
 
-#Assign each vector to a cluster, return 
-#in the form of a dictionary
 def assignToCluster(vec, centroids, i):
   minK = numpy.argmin([distance.euclidean(vec, centroid) for centroid in centroids])
-  return {'cluster': minK, 'vec': vec, 'class': i, 'change': True}
+  return {'cluster': minK, 'vec': vec, 'class': (i // 100), 'change': True}
   
-#filter function to return vecs from a cluster
 def isInCluster(vec, cluster):
   return (True if cluster == vec['cluster'] else False)
   
-#Get the average of all vectors in some cluster
-def getAverageOfCluster(clusterVecs, k):
-  #print(clusterVecs[0])
-  cluster = [vec for vec in clusterVecs if isInCluster(vec, cluster=k)]
-  #Get length of vectors
-  length = len(clusterVecs[0]['vec'])
+def getAverageOfCluster(clusterVecs, k)  :
+  cluster = copy.deepcopy([x for x in clusterVecs if isInCluster(x, cluster=k)])
+  return copy.deepcopy([(sum([point['vec'][i] for point in cluster]) / len(cluster)) for i in range(0, len(cluster[0]['vec']))])
   
- 
-  return  [(sum([point['vec'][i] for point in cluster]) / len(cluster)) for i in range(0, length)]  
-  #a = numpy.array([vec['vec'] for vec in cluster])
-  #print(numpy.mean(a, axis=0))
-
-#Reassign a vector to the closest cluster given the means
 def reassignToCluster(vec, clusterMeans):
-  closest = numpy.argmin([distance.euclidean(vec['vec'], clusterMean) for clusterMean in clusterMeans])
-  if closest == vec['cluster']:
+  clus = numpy.argmin([distance.euclidean(vec['vec'], clusterMean) for clusterMean in clusterMeans])
+  if vec['cluster'] == clus:
     vec['change'] = False
   else:
     vec['change'] = True
+  vec['cluster'] = clus
   return vec
+def add(x, y): return (x + y)  
 
-def checkForChange(vec):
-  return vec['change']
+def getAccuracy(counter):
+  maxApp = 0
+  maxApps = []
+  for x in counter:
+    for k, v in x.items():
+      if v != 0 and v > maxApp:
+        maxApp = v
+    maxApps.append(maxApp)
+    maxApp = 0
+  return (functools.reduce(add, maxApps) / 6)
+  
+def printOccurences(counter):
+  
+  for i, x in enumerate(counter):
+    print('\nCluster ' + str(i + 1) + ' occurences: ')
+    for k, v in x.items():
+      if v != 0:
+        print("Class " + str(k + 1), ": ", v, "%")
+
+def getOccurences(clusteredVecs):
+  occurences = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
+  counter = [(copy.deepcopy(occurences)), (copy.deepcopy(occurences)),
+            (copy.deepcopy(occurences)), (copy.deepcopy(occurences)),
+            (copy.deepcopy(occurences)), (copy.deepcopy(occurences))]
+  res = [[] for i in range(6)]
+        
+  for vec in (clusteredVecs):
+    res[vec['cluster']].append(vec['class'])
+  for i, r in enumerate(res):
+    for c in r:
+      counter[i][c] = counter[i][c] + 1
+  return counter
 
 '''
 File input and cmd line arg helper functions
@@ -93,25 +84,24 @@ def readFile(fileName):
   #Split lines from file
   lines = open(fileName).read().split("\n")
   reader = csv.reader(lines)
-  
   #Split points now
+  
   data = [ point for point in [line for line in reader]]
-  #Pop last row out, contains empty list
+  #Pop last row out, empty list
   data.pop()
   
   #convert each list of string vals to a list of floats
-  fData = list(map(convertListToFloats, data))
+  fData = list( map(convertListToFloats, data))
   return fData
   
 def convertListToFloats(row):
   strings = row[0].split()
-  return list(map(convertStringToFloat, strings))
+  return list( map(convertStringToFloat, strings))
   
 def convertStringToFloat(string):
   return float(string)
   
-
 def getUserInput():
-  k = int(sys.argv[1])
-  runs = int(sys.argv[2])
-  return {'k': k, 'runs': runs}
+  #k = int(sys.argv[1])
+  runs = int(sys.argv[1])
+  return {'runs': runs}
--- final/main.py
+++ final/main.py
@@ -17,33 +17,15 @@ if __name__ == '__main__':
 # number 
   #Get data from file, and cmd line args
   data = readFile('data.txt')
-  #args = getUserInput() 
+  args = getUserInput() 
   
   #Run the kmeans function a bunch of times, save results
   acc = 0
-  #for x in range(args['runs']):
-  clusteredVecs = kMeans(data, 6, 5)
-  occurences = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
-  counter = [(copy.deepcopy(occurences)), (copy.deepcopy(occurences)),
-            (copy.deepcopy(occurences)), (copy.deepcopy(occurences)),
-            (copy.deepcopy(occurences)), (copy.deepcopy(occurences))]
-#   for i, vec in enumerate(clusteredVecs):
-#       counter[i//100][vec['cluster']] = 1 + counter[i//100][vec['cluster']]
-#   print("Ground truth results")
-#   for i, cluster in enumerate(counter):
-#       print("Class ", i)
-#       for k, v in cluster.items():
-#           if v != 0:
-#               print(k, ": ", v, "%")
-              
-  #for x in range(0, 100):
-   #   print(clusteredVecs[x]['cluster'])
-   # acc = acc + (len([vec for vec in clusteredVecs if vec['class'] == vec['cluster']]) / len(clusteredVecs))
-  #print(str((acc / args['runs']) * 100))
-
-  
-    
-    
-  
-
+  for x in range(args['runs']):
+    clusteredVecs = copy.deepcopy(kMeans(data, 6))
+    acc = acc + getAccuracy(getOccurences(clusteredVecs))
+      
+  print('\n\tRuns: ' + str(args['runs']))
+  print('\tAccuracy: ' + str(acc/args['runs']))
+  print('')
 
